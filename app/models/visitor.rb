cd36c8f7b568515e61c2a17dd77b6180d6fd6580
ponizi <ponalexis@gmail.com>
2019-09-30 23:58:17 +0200

    First commit emma

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,7 +1,3 @@
 class Visitor < ApplicationRecord
-  validates :first_name, presence:true
-  validates :last_name, presence:true
-  validates :phone_number, presence:true
   validates :email, presence:true
-  validates :message, presence:true
 end
